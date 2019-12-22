e1a54a88691399c26a03b5a8a0e9ffbe3b540c95
AnuraktaNayak <Anurakta.it@gmail.com>
2019-12-21 22:41:33 -0500

    Update App.java
    added thread.sleep logic

--- src/main/java/com/test/project/MavenEclipseProject/App.java
+++ src/main/java/com/test/project/MavenEclipseProject/App.java
@@ -6,8 +6,12 @@ package com.test.project.MavenEclipseProject;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello World!" );
+        
+        long start = System.currentTimeMillis();
+        Thread.sleep(200000)
+        System.out.println("Sleep time in ms = "+(System.currentTimeMillis()-start));
     }
 }
